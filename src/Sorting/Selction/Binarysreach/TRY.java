cea2b2f72574bf25594cbfa951e55e1f5d133c3f
shrikant c <shrikantchavan9404@gmail.com>
2023-08-21 09:51:16 +0530

    changing file location

--- src/Sorting/Selction/Binarysreach/TRY.java
+++ src/Sorting/Selction/Binarysreach/TRY.java
@@ -2,52 +2,38 @@ package Sorting.Selction.Binarysreach;
 
 
 
-import java.util.Arrays;
-import java.util.Random;
-class TRY {
 
+public class TRY {
+    public static int binary(int[]arr){
+        int start =0;
+        int end = arr.length-1;
 
-    public static int[] searchRange(int[] nums, int target) {
-        int [] arr={-1,-1};
-        int first =value(nums,target,true);
-        int last =value(nums,target,false);
-        arr[0]=first;
-        arr[1]=last;
+        while(end>start){
+            int mid =start+(end-start)/2;
 
-        return arr;
+            if(arr[mid]>arr[mid+1]){
 
+                end=mid;
+            }
+            else {
 
-    }
+                start=mid+1;
 
-
-    static int  value(int[] nums, int target, boolean b) {
-        int start = 0;
-        int ans = -1;
-        int end = nums.length- 1;
-        while (end >= start) {
-            int mid = start + (end - start) / 2;
-            if (nums[mid] > target) {
-                end = mid - 1;
-            } else if (nums[mid] < target) {
-                start = mid + 1;
-            } else {
-                ans = mid;
-                if (b) {
-                    end = mid - 1;
-                } else {
-                    start = mid + 1;
-                }
             }
 
         }
 
-
-        return ans;
-
+        return start;
     }
+
 
     public static void main(String[] args) {
-        int [] arr={1,2,3,5,7,7,7,8,8};
-        System.out.println(Arrays.toString(searchRange(arr,7)));
+
+        int[] arr = {1,2,1,3,5,6,4};
+        System.out.println(binary(arr));
     }
+
+
+
+
 }
